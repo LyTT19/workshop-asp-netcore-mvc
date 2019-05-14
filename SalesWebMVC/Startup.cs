6a6f61155531e5fd37d71d731b83d2a767a0b86d
Jackson Callado <34450896+LyTT19@users.noreply.github.com>
2019-05-14 00:28:36 -0300

    Validation

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Globalization/
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
